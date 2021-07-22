50212376134eff4e3cf605765cfb484cc341d728
Anton <enitefall81@gmail.com>
2021-07-22 14:10:20 -0700

    Display latitude component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import SeasonDisplay from "./SeasonDisplay";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {lat: null}
+
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => this.setState({lat: position.coords.latitude}),
+        (err) => console.log(err))
+  }
+
+  render() {
+    return <div>
+      {this.state.lat}
+      {console.log(this.state)}
     </div>
-  );
+  }
 }
 
 export default App;
---
+++ src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+let SeasonDisplay = () => {
+  return <div>
+    Season display
+  </div>
+}
+
+export default SeasonDisplay
