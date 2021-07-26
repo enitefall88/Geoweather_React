2843a531cdc0e535be1e33dd76f5de2689de58b6
Anton <enitefall81@gmail.com>
2021-07-26 13:27:51 -0700

    Add weather icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+let getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter'
+  } else {
+    return lat > 0 ? 'winter' : 'summer'
+  }
+}
+
 let SeasonDisplay = (props) => {
+  let season = getSeason(props.lat, new Date().getMonth())
+  let icon = season === 'winter' ? 'snowflake' : 'sun'
+console.log(season)
   return <div>
-  {props.lat}
-  </div>
+    <i className={`${icon} icon`}></i>
+    <h1>
+      {season === 'winter' ? 'Burr, it is chilly' : 'Let`s hit the beach'}
+    </h1>
+       <i className={`${icon} icon`}></i>
+    </div>
 }
 
 export default SeasonDisplay
