db739a324b707557a3ffd6788f90fa8abaa916e6
Anton <enitefall81@gmail.com>
2021-07-28 11:25:11 -0700

    Refactor with helper function in render conditional

--- src/App.js
+++ src/App.js
@@ -7,19 +7,7 @@ class App extends React.Component {
   state = {lat: null, errorMessage: ''}
 
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-        (
-            position) => this.setState({lat: position.coords.latitude}),
-        (
-            err) => this.setState({errorMessage: err.message})
-        //console.log(err)
-
-    )
-
-  }
-
-  render() {
+ renderContent () {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>
         Error: {this.state.errorMessage}
@@ -33,8 +21,26 @@ class App extends React.Component {
     }
 
     return <div>
-    <Spinner/>
+      <Spinner message="Please accept location request"/>
     </div>
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        (
+            position) => this.setState({lat: position.coords.latitude}),
+        (
+            err) => this.setState({errorMessage: err.message})
+        //console.log(err)
+
+    )
+
+  }
+
+  render() {
+  return <div className="border red">
+    {this.renderContent()}
+  </div>
   }
 }
 
--- src/Spinner.js
+++ src/Spinner.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-export default function Spinner() {
+export default function Spinner(props) {
 return <div className="ui active inverted dimmer">
-      <div className="ui text loader">Loading</div>
+      <div className="ui text loader">{props.message}</div>
    </div>
+}
+
+Spinner.defaultProps = {
+  message: 'Loading..'
 }
